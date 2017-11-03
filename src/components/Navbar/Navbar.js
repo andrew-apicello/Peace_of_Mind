146b3ac1288e9c2b49f427339aa6c3f791de1280
annalisa leote <amleote@gmail.com>
2017-11-03 11:44:33 -0400

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,13 +9,12 @@ const Navbar = () =>
     <li className={window.location.pathname === "/createprofile" ? "active" : ""}>
       <Link to="/createprofile">Create Profile</Link>
     </li>
-<<<<<<< HEAD:src/components/Navbar/Navbar.js
+
      <li className={window.location.pathname === "/logout" ? "active" : ""}>
       <Link to="/createprofile">Logout</Link>
-=======
-    <li className={window.location.pathname === "/logout" ? "active" : ""}>
-      <Link to="/logout">Logout</Link>
->>>>>>> 15e9733f18dd2fdd19772f86a9174892b04de3e5:src/components/Navbar/Navbar.js
+
+  
+
     </li>
      
    
